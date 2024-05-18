7d14ef9ae27de06f4bfc41ae4857ef981a6d23e2
lightningRalf <lightningralf@skiff.com>
2024-05-18 20:22:12 +0200

    change mocks

--- __mocks__/obsidian.ts
+++ __mocks__/obsidian.ts
@@ -1,42 +1,11 @@
-// This file is a mock implementation of the Obsidian API for Jest testing
-// It simulates the Obsidian environment for ExcalidrawAutomate, ExcalidrawView, and ExcalidrawData components
-
-const mockVault = {
-  getAbstractFileByPath: jest.fn(),
-  read: jest.fn(),
-  create: jest.fn(),
-  modify: jest.fn(),
-  delete: jest.fn(),
-};
-
-const mockMetadataCache = {
-  getCache: jest.fn(),
-  on: jest.fn(),
-};
-
-const mockWorkspace = {
-  getActiveFile: jest.fn(),
-  onLayoutReady: jest.fn(),
-  on: jest.fn(),
-};
-
-const mockApp = {
-  vault: mockVault,
-  metadataCache: mockMetadataCache,
-  workspace: mockWorkspace,
-  plugins: {
-    plugins: {
-      "obsidian-excalidraw-plugin": {
-        getExcalidrawAPI: jest.fn(),
-      },
-    },
-  },
-};
+import { AppMock } from "./obsidian/App";
+import { PluginMock } from "./obsidian/Plugin";
+import { ModalMock } from "./obsidian/Modal";
+import { PluginSettingTabMock } from "./obsidian/PluginSettingTab";
 
 module.exports = {
-  Vault: jest.fn(() => mockVault),
-  MetadataCache: jest.fn(() => mockMetadataCache),
-  Workspace: jest.fn(() => mockWorkspace),
-  App: jest.fn(() => mockApp),
-  mockApp, mockVault, mockMetadataCache, mockWorkspace // Exporting the mocks directly for easier import in tests
+	App: AppMock,
+	Plugin: PluginMock,
+	Modal: ModalMock,
+	PluginSettingTab: PluginSettingTabMock,
 };
