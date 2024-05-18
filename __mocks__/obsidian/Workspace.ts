dccaddde32ca0b688012da6c9285a7287df40663
lightningRalf <lightningRalf@proton.me>
2024-05-18 23:59:14 +0200

    Update Workspace.ts
    change (name to (eventName

--- __mocks__/obsidian/Workspace.ts
+++ __mocks__/obsidian/Workspace.ts
@@ -17,52 +17,52 @@ export class WorkspaceMock implements Workspace {
   }
 
   on(eventName: 'quick-preview', callback: (file: TFile, data: string) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'resize', callback: () => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'active-leaf-change', callback: (leaf: WorkspaceLeaf | null) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'file-open', callback: (file: TFile | null) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'layout-change', callback: () => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'window-open', callback: (win: WorkspaceWindow, window: Window) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'window-close', callback: (win: WorkspaceWindow, window: Window) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'css-change', callback: () => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'file-menu', callback: (menu: Menu, file: TAbstractFile, source: string, leaf?: WorkspaceLeaf) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'files-menu', callback: (menu: Menu, files: TAbstractFile[], source: string, leaf?: WorkspaceLeaf) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'url-menu', callback: (menu: Menu, url: string) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'editor-menu', callback: (menu: Menu, editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'editor-change', callback: (editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'editor-paste', callback: (evt: ClipboardEvent, editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'editor-drop', callback: (evt: DragEvent, editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
   on(eventName: 'quit', callback: (tasks: Tasks) => any, ctx?: any): EventRef {
-      return new MockEventRef(name, callback, ctx);
+      return new MockEventRef(eventName, callback, ctx);
   }
 
   off(eventName: string, callback: (...args: any[]) => any, ctx?: any): void {
