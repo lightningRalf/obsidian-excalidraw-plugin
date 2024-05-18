a85e421e3b41af31fe662d3f7d638dbfef5c9bb8
lightningRalf <lightningRalf@proton.me>
2024-05-18 22:47:07 +0200

    Update Workspace.ts
    add EventRef as namedImport from obsidian
    add MockEventRef
    fix on

--- __mocks__/obsidian/Workspace.ts
+++ __mocks__/obsidian/Workspace.ts
@@ -1,4 +1,5 @@
-import type { Workspace, WorkspaceLeaf } from "obsidian";
+import type { Workspace, WorkspaceLeaf, EventRef } from "obsidian";
+import { MockEventRef } from './mockHelpers';
 
 export class WorkspaceMock implements Workspace {
   activeLeaf: WorkspaceLeaf | null = null;
@@ -15,8 +16,8 @@ export class WorkspaceMock implements Workspace {
     throw new Error("Method not implemented.");
   }
 
-  on(name: string, callback: (...args: any[]) => any, ctx?: any): void {
-    throw new Error("Method not implemented.");
+  on(name: "quick-preview" | "resize" | "active-leaf-change" | "file-open", callback: (...args: any[]) => any, ctx?: any): EventRef {
+      return new MockEventRef(name, callback, ctx);
   }
 
   off(name: string, callback: (...args: any[]) => any, ctx?: any): void {
