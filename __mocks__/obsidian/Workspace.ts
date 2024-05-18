ea09d588f4f3b67aa2d5d6be72839a659bad7af3
lightningRalf <lightningRalf@proton.me>
2024-05-19 00:21:56 +0200

    Update Workspace.ts
    add Overload for events

--- __mocks__/obsidian/Workspace.ts
+++ __mocks__/obsidian/Workspace.ts
@@ -16,7 +16,43 @@ export class WorkspaceMock implements Workspace {
     throw new Error("Method not implemented.");
   }
 
-  on(eventName: "quick-preview" | "resize" | "active-leaf-change" | "file-open" | "layout-change" | "window-open" | "window-close" | "css-change" | "file-menu" | "files-menu" | "url-menu" | "editor-menu" | "editor-change" | "editor-paste" | "editor-drop" | "quit", callback: (...args: any[]) => any, ctx?: any): EventRef {
+  //Overload for events
+  on(eventName: 'quick-preview', callback: (file: TFile, data: string) => any, ctx?: any): EventRef;
+  on(eventName: 'resize', callback: () => any, ctx?: any): EventRef;
+  on(eventName: 'active-leaf-change', callback: (leaf: WorkspaceLeaf | null) => any, ctx?: any): EventRef;
+  on(eventName: 'file-open', callback: (file: TFile | null) => any, ctx?: any): EventRef;
+  on(eventName: 'layout-change', callback: () => any, ctx?: any): EventRef;
+  on(eventName: 'window-open', callback: (win: WorkspaceWindow, window: Window) => any, ctx?: any): EventRef;
+  on(eventName: 'window-close', callback: (win: WorkspaceWindow, window: Window) => any, ctx?: any): EventRef;
+  on(eventName: 'css-change', callback: () => any, ctx?: any): EventRef;
+  on(eventName: 'file-menu', callback: (menu: Menu, file: TAbstractFile, source: string, leaf?: WorkspaceLeaf) => any, ctx?: any): EventRef;
+  on(eventName: 'files-menu', callback: (menu: Menu, files: TAbstractFile[], source: string, leaf?: WorkspaceLeaf) => any, ctx?: any): EventRef;
+  on(eventName: 'url-menu', callback: (menu: Menu, url: string) => any, ctx?: any): EventRef;
+  on(eventName: 'editor-menu', callback: (menu: Menu, editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef; 
+  on(eventName: 'editor-change', callback: (editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef;
+  /**
+   * Triggered when the editor receives a paste event.
+   * Check for `evt.defaultPrevented` before attempting to handle this event, and return if it has been already handled.
+   * Use `evt.preventDefault()` to indicate that you've handled the event.
+   * @public
+   */
+  on(eventName: 'editor-paste', callback: (evt: ClipboardEvent, editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef;
+  /**
+   * Triggered when the editor receives a drop event.
+   * Check for `evt.defaultPrevented` before attempting to handle this event, and return if it has been already handled.
+   * Use `evt.preventDefault()` to indicate that you've handled the event.
+   * @public
+   */
+  on(eventName: 'editor-drop', callback: (evt: DragEvent, editor: Editor, info: MarkdownView | MarkdownFileInfo) => any, ctx?: any): EventRef;
+  /**
+   * Triggered when the app is about to quit. Not guaranteed to actually run.
+   * Perform some best effort cleanup here.
+   * @public
+   */
+  on(eventName: 'quit', callback: (tasks: Tasks) => any, ctx?: any): EventRef;
+
+  // General method implementation
+  on(eventName: string, callback: (...args: any[]) => any, ctx?: any): EventRef {
     // Your code logic here. Return a mock EventRef.
     return new MockEventRef(eventName, callback, ctx);
 }
